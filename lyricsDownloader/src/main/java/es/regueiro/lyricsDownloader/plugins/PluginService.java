17d6ee12d6db27d781522b06bff77b5220099747
regueiro <santi@regueiro.es>
2013-02-21 20:17:34 +0100

    Formatted plugin related files

--- lyricsDownloader/src/main/java/es/regueiro/lyricsDownloader/plugins/PluginService.java
+++ lyricsDownloader/src/main/java/es/regueiro/lyricsDownloader/plugins/PluginService.java
@@ -3,9 +3,9 @@ package es.regueiro.lyricsDownloader.plugins;
 import es.regueiro.lyricsDownloader.api.plugins.Plugin;
 
 import java.util.Iterator;
- 
-public interface PluginService
-{
-    Iterator<Plugin> getPlugins();
-    void initPlugins();
+
+public interface PluginService {
+	Iterator<Plugin> getPlugins();
+
+	void initPlugins();
 }
--- lyricsDownloader/src/main/java/es/regueiro/lyricsDownloader/plugins/StandardPluginService.java
+++ lyricsDownloader/src/main/java/es/regueiro/lyricsDownloader/plugins/StandardPluginService.java
@@ -5,45 +5,38 @@ import java.util.ServiceLoader;
 import java.util.logging.Logger;
 
 import es.regueiro.lyricsDownloader.api.plugins.Plugin;
- 
-public class StandardPluginService implements PluginService
-{
-    private static StandardPluginService pluginService;
-    private ServiceLoader<Plugin> serviceLoader;
-    private Logger logger = Logger.getLogger(getClass().getName());
- 
-    private StandardPluginService()
-    {
-        //load all the classes in the classpath that have implemented the interface
-        serviceLoader = ServiceLoader.load(Plugin.class);
-    }
- 
-    public static StandardPluginService getInstance()
-    {
-        if(pluginService == null)
-        {
-            pluginService = new StandardPluginService();
-        }
-        return pluginService;
-    }
- 
-    public Iterator<Plugin> getPlugins()
-    {
-        return serviceLoader.iterator();
-    }
- 
-    public void initPlugins()
-    {
-        Iterator<Plugin> iterator = getPlugins();
-        if(!iterator.hasNext())
-        {
-            logger.info("No plugins were found!");
-        }
-        while(iterator.hasNext())
-        {
-            Plugin plugin = iterator.next();
-            logger.info("Initializing the plugin " + plugin.getName());
-            plugin.init();
-        }
-    }
+
+public class StandardPluginService implements PluginService {
+	private static StandardPluginService pluginService;
+	private ServiceLoader<Plugin> serviceLoader;
+	private Logger logger = Logger.getLogger(getClass().getName());
+
+	private StandardPluginService() {
+		// load all the classes in the classpath that have implemented the
+		// interface
+		serviceLoader = ServiceLoader.load(Plugin.class);
+	}
+
+	public static StandardPluginService getInstance() {
+		if (pluginService == null) {
+			pluginService = new StandardPluginService();
+		}
+		return pluginService;
+	}
+
+	public Iterator<Plugin> getPlugins() {
+		return serviceLoader.iterator();
+	}
+
+	public void initPlugins() {
+		Iterator<Plugin> iterator = getPlugins();
+		if (!iterator.hasNext()) {
+			logger.info("No plugins were found!");
+		}
+		while (iterator.hasNext()) {
+			Plugin plugin = iterator.next();
+			logger.info("Initializing the plugin " + plugin.getName());
+			plugin.init();
+		}
+	}
 }
